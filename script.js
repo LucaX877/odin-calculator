c6df90f79708f4fffb7486574a89828925230110
Luca <171629213+LucaX877@users.noreply.github.com>
2025-08-02 22:16:14 +0100

    Add event listeners for buttons
    Calculator can now perform basic equations which involve unit numbers

--- script.js
+++ script.js
@@ -19,13 +19,40 @@ function divide(num1,num2) {
 }
 
 function operate(operator,num1,num2) {
-    if (operator == "+") add(num1,num2);
-    else if (operator == "-") subtract(num1,num2);
-    else if (operator == "*") multiply(num1,num2);
-    else if (operator == "/") divide(num1,num2);
+    if (operator == "+") return add(num1,num2);
+    else if (operator == "-") return subtract(num1,num2);
+    else if (operator == "*") return multiply(num1,num2);
+    else if (operator == "/") return divide(num1,num2);
 }
 
-let number1 = parseInt(prompt("Enter first number: "));
-let number2 = parseInt(prompt("Enter second number: "));
-let operator = prompt("Enter operator: ");
-operate(operator,number1,number2);
+displayEquation = document.querySelector(".display-equation")
+
+numberButtons = document.querySelectorAll(".number-button")
+for (let numberButton of numberButtons) {
+    numberButton.addEventListener("click", () => {
+        console.log(displayEquation.textContent.length);
+        if (displayEquation.textContent.length == 1) displayEquation.textContent = numberButton.textContent;
+        else displayEquation.textContent += " " + numberButton.textContent;
+    })
+}
+
+operatorButtons = document.querySelectorAll(".operator-button");
+for (let operatorButton of operatorButtons) {
+    operatorButton.addEventListener("click", () => {
+        displayEquation.textContent += " " + operatorButton.textContent
+    })
+}
+
+equalButton = document.querySelector(".equal-button");
+equalButton.addEventListener("click", () => {
+    let items = displayEquation.textContent.split(" ");
+    let number1 = parseInt(items[0]);
+    let operator = items[1];
+    let number2 = parseInt(items[2]);
+    displayEquation.textContent = operate(operator,number1,number2);
+})
+
+clearButton = document.querySelector(".clear-button");
+clearButton.addEventListener("click", () => {
+    displayEquation.textContent = 0;
+})
